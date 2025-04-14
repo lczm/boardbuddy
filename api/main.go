2fd7e02746e279f2d1433da742ef86dc79104b01
lczm <zemingchua@gmail.com>
2025-04-14 20:01:26 +0800

    add cli to sync kilter db

--- api/main.go
+++ api/main.go
@@ -1,18 +1,49 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/exec"
 
 	"github.com/lczm/boardbuddy/api/config"
 	"github.com/lczm/boardbuddy/api/routes"
+	"github.com/spf13/cobra"
 )
 
 func main() {
-	// init, get all db connections
-	config.ConnectKilterDB()
+	var syncKilter bool
 
-	// get the router and run
-	r := routes.SetupRoutes()
-	log.Fatal(http.ListenAndServe(":8082", r))
+	rootCmd := &cobra.Command{
+		Use: "boardbuddy",
+		Run: func(cmd *cobra.Command, args []string) {
+			if syncKilter {
+				// Execute the database sync command
+				syncKilterCommand := exec.Command("boardlib", "database", "kilter", "kilter.db")
+				syncKilterCommand.Stdout = os.Stdout
+				syncKilterCommand.Stderr = os.Stderr
+				err := syncKilterCommand.Run()
+				if err != nil {
+					log.Fatalf("Failed to sync database: %v", err)
+				}
+				fmt.Println("Kilter Database sync completed")
+				return
+			}
+			// init, get all db connections
+			config.ConnectKilterDB()
+
+			// get the router and run
+			r := routes.SetupRoutes()
+			log.Fatal(http.ListenAndServe(":8082", r))
+		},
+	}
+
+	// specify the flag needed
+	rootCmd.Flags().BoolVar(&syncKilter, "sync-kilter", false, "Sync the Kilter database")
+
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatalf("Error executing command: %v", err)
+		os.Exit(1)
+	}
 }
