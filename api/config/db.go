a74be51eb850a695605c73796f78a87973aa352b
lczm <zemingchua@gmail.com>
2025-05-26 22:41:13 +0800

    use sqlite3 dsn to speed up queries

--- api/config/db.go
+++ api/config/db.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"fmt"
 	"log"
 
 	"gorm.io/driver/sqlite"
@@ -11,10 +12,21 @@ import (
 var KilterDB *gorm.DB
 
 func ConnectKilterDB() {
+	// Use WAL mode for better concurrency and performance
+	dsn := fmt.Sprintf(
+		"file:%s?_journal_mode=WAL&"+
+			"_synchronous=NORMAL&"+
+			"_cache_size=20000&"+ // ~80 MiB cache
+			"_mmap_size=157286400&"+ // cap at 150 MiB
+			"_temp_store=MEMORY&"+
+			"_foreign_keys=ON",
+		"kilter.db",
+	)
+
 	var err error
 	// TODO : add more in the future
 	// only kilter for now
-	KilterDB, err = gorm.Open(sqlite.Open("kilter.db"), &gorm.Config{
+	KilterDB, err = gorm.Open(sqlite.Open(dsn), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info),
 	})
 	if err != nil {
